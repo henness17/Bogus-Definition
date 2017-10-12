76272431cc2adc1b48ce7801e298e63731349c37
Ryan Henness <henness17@users.noreply.github.com>
2017-10-11 21:53:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -87,9 +87,3 @@ function capitalizeFirstLetter(string) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-// Keep the Heroku server awake
-var http = require("http");
-setInterval(function() {
-    http.get("http://bogus-definition.herokuapp.com");
-}, 1800000); // Ping server every 30 mins
